2ac4b257d89851a4b1e4659908a932212a79c116
HajiAli <senayousofiali@gmail.com>
2024-07-23 12:25:58 +0430

    added: spinners, api routes

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,14 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "1R",
-  description: "Sharing your one regret might save someone else a lot of time.",
+  title: "Quotes",
+  description: "Add your first open-source quote",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={{backgroundColor: "white"}} className={inter.className}>{children}</body>
     </html>
   );
 }
---
+++ data/quotes.js
@@ -0,0 +1,50 @@
+export const quotes = [
+    {
+      "quote": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+      "author": "Thomas Edison"
+    },
+    {
+      "quote": "You can observe a lot just by watching.",
+      "author": "Yogi Berra"
+    },
+    {
+      "quote": "A house divided against itself cannot stand.",
+      "author": "Abraham Lincoln"
+    },
+    {
+      "quote": "Difficulties increase the nearer we get to the goal.",
+      "author": "Johann Wolfgang von Goethe"
+    },
+    {
+      "quote": "Fate is in your hands and no one elses",
+      "author": "Byron Pulsifer"
+    },
+    {
+      "quote": "Be the chief but never the lord.",
+      "author": "Lao Tzu"
+    },
+    {
+      "quote": "Nothing happens unless first we dream.",
+      "author": "Carl Sandburg"
+    },
+    {
+      "quote": "Well begun is half done.",
+      "author": "Aristotle"
+    },
+    {
+      "quote": "Life is a learning experience, only if you learn.",
+      "author": "Yogi Berra"
+    },
+    {
+      "quote": "Self-complacency is fatal to progress.",
+      "author": "Margaret Sangster"
+    },
+    {
+      "quote": "Peace comes from within. Do not seek it without.",
+      "author": "Buddha"
+    },
+    {
+      "quote": "What you give is what you get.",
+      "author": "Byron Pulsifer"
+    },
+  ]
